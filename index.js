ffc1bb0db7864dce6ccaeb93b6c4072e7abe7757
Hasindu Koshitha <hasindu.learn.online@gmail.com>
2026-03-19 17:41:46 +0530

    add

--- index.js
+++ index.js
@@ -32,16 +32,6 @@ const allowedOrigins = [
 // Serve Swagger UI
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-console.log("USER_SERVICE_URL:", process.env.USER_SERVICE_URL);
-console.log("EVENT_SERVICE_URL:", process.env.EVENT_SERVICE_URL);
-console.log("BOOKING_SERVICE_URL:", process.env.BOOKING_SERVICE_URL);
-console.log("PAYMENT_SERVICE_URL:", process.env.PAYMENT_SERVICE_URL);
-console.log("NOTIFICATION_SERVICE_URL:", process.env.NOTIFICATION_SERVICE_URL);
-console.log("FRONTEND_URL:", process.env.FRONTEND_URL);
-console.log("PORT:", process.env.PORT);
-console.log("BASE_URL:", process.env.BASE_URL);
-console.log("JWT_EXPIRES:", process.env.JWT_EXPIRES);
-
 function authenticateTokenUnlessPublicUserRoute(req, res, next) {
   if (publicUserPaths.has(req.path)) {
     return next();
