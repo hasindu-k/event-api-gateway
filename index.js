58cf30b3709b21964d2cfa70713d8b355a4d8abd
Hasindu Koshitha <hasindu.learn.online@gmail.com>
2026-03-19 17:45:27 +0530

    refactor: update log message for incoming requests

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  console.log("Incoming request:", req.method, req.url);
+  console.log("Incoming request updated:", req.method, req.url);
   next();
 });
 
